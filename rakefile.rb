ccbc245a4d277cb50e550e0e3cc713a9fd4c13a6
Rick <rick@cloudscaling.com>
2013-07-06 20:14:47 -0700

    updated for new defaults

--- rakefile.rb
+++ rakefile.rb
@@ -2,8 +2,12 @@ require 'buildar/tasks'
 require 'rake/testtask'
 
 Buildar.conf(__FILE__) do |b|
+  b.use_version_file = true
+  b.version_filename = 'VERSION'
+  b.use_manifest_file = true
+  b.manifest_filename = 'MANIFEST.txt'
   b.gemspec.name = 'buildar'
-  b.gemspec.summary = 'Buildar crept inside your rakefile and scratched some tasks'
+  b.gemspec.summary = 'Buildar crept inside your rakefile and scratched upon the tasking post'
   b.gemspec.description = 'Buildar helps automate the release process with versioning, building, packaging, and publishing.  Optional git integration'
   b.gemspec.author = 'Rick Hull'
   b.gemspec.homepage = 'https://github.com/rickhull/buildar'
