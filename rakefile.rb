38496552063ae93a77e5e191193df866c4ed814f
Rick <rick@cloudscaling.com>
2013-07-06 23:25:14 -0700

    updated per new defaults

--- rakefile.rb
+++ rakefile.rb
@@ -6,13 +6,15 @@ Buildar.conf(__FILE__) do |b|
   b.version_filename  = 'VERSION'
   b.use_manifest_file = true
   b.manifest_filename = 'MANIFEST.txt'
-  b.gemspec.name      = 'buildar'
-  b.gemspec.summary   = 'Buildar crept inside your rakefile and scratched upon the tasking post'
-  b.gemspec.description = 'Buildar helps automate the release process with versioning, building, packaging, and publishing.  Optional git integration.'
+  b.use_git           = true
+  b.publish[:rubygems] =  true
+  b.gemspec.name     = 'buildar'
+  b.gemspec.summary  = 'Buildar crept inside your rakefile and scratched upon the tasking post'
   b.gemspec.author   = 'Rick Hull'
   b.gemspec.homepage = 'https://github.com/rickhull/buildar'
   b.gemspec.license  = 'MIT'
   b.gemspec.has_rdoc = true
+  b.gemspec.description = 'Buildar helps automate the release process with versioning, building, packaging, and publishing.  Optional git integration.'
   b.gemspec.add_runtime_dependency        "rake", ">= 8" # guess?
   b.gemspec.add_development_dependency "buildar", "~> 1.0"
 end
