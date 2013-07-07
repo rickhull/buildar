bb0e54e57ae3da938d5b0b117b5edae9165c201e
Rick <rick@cloudscaling.com>
2013-07-06 21:46:32 -0700

    nix EOF

--- rakefile.rb
+++ rakefile.rb
@@ -8,14 +8,12 @@ Buildar.conf(__FILE__) do |b|
   b.manifest_filename = 'MANIFEST.txt'
   b.gemspec.name      = 'buildar'
   b.gemspec.summary   = 'Buildar crept inside your rakefile and scratched upon the tasking post'
-  b.gemspec.description = <<EOF
-Buildar helps automate the release process with versioning, building, packaging, and publishing.  Optional git integration.
-EOF
+  b.gemspec.description = 'Buildar helps automate the release process with versioning, building, packaging, and publishing.  Optional git integration.'
   b.gemspec.author   = 'Rick Hull'
   b.gemspec.homepage = 'https://github.com/rickhull/buildar'
   b.gemspec.license  = 'MIT'
   b.gemspec.has_rdoc = true
-  b.gemspec.add_runtime_dependency        "rake", ">= 5" # guess?
+  b.gemspec.add_runtime_dependency        "rake", ">= 8" # guess?
   b.gemspec.add_development_dependency "buildar", "~> 1.0"
 end
 
