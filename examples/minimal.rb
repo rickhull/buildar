5eaeedeb680237b90e3ab2b938678dc144bbb0a4
Rick <rick@cloudscaling.com>
2013-07-06 20:33:07 -0700

    per README.md

--- examples/minimal.rb
+++ examples/minimal.rb
@@ -1,10 +1,9 @@
 require 'buildar/tasks'
 
 Buildar.conf(__FILE__) do |b|
-  b.use_version_file = false
-  b.use_manifest_file = false
-  b.gemspec.version = '0'
-  b.gemspec.files = File.join(b.root, File.basename(__FILE__))
+  b.name = 'Example'             # optional, inferred from directory
+  b.gemspec.version = '1.0'      # required, unless b.use_version_file
+  b.gemspec.files = ['Rakefile'] # required, unless b.use_manifest_file
 end
 
 # make sure you have a task named :test, even if it's empty
