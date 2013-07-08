1eff2ca1006e5abebae1bfa245260d9aa6a03af8
Rick <rick@cloudscaling.com>
2013-07-07 18:55:03 -0700

    cleanup

--- rakefile.rb
+++ rakefile.rb
@@ -8,17 +8,7 @@ Buildar.conf(__FILE__) do |b|
   b.use_manifest_file = true
   b.manifest_filename = 'MANIFEST.txt'
   b.use_git           = true
-  b.publish[:rubygems] =  true
-
-#  b.gemspec.summary  = 'Buildar crept inside your rakefile and scratched upon the tasking post'
-#  b.gemspec.author   = 'Rick Hull'
-#  b.gemspec.homepage = 'https://github.com/rickhull/buildar'
-#  b.gemspec.license  = 'MIT'
-#  b.gemspec.has_rdoc = true
-#  b.gemspec.description = 'Buildar helps automate the release process with versioning, building, packaging, and publishing.  Optional git integration.'
-
-#  b.gemspec.add_runtime_dependency        "rake", ">= 8" # guess?
-#  b.gemspec.add_development_dependency "buildar", "~> 1.0"
+  b.publish[:rubygems] = true
 end
 
 Rake::TestTask.new :test do |t|
