191b8b82ee521f5f966238a3f766ce6a8a5c1fc7
Rick <rick@cloudscaling.com>
2013-07-07 19:11:13 -0700

    use_manifest_file = false; doing magic in gemspec

--- rakefile.rb
+++ rakefile.rb
@@ -5,8 +5,6 @@ Buildar.conf(__FILE__) do |b|
   b.name = 'buildar'
   b.use_version_file  = true
   b.version_filename  = 'VERSION'
-  b.use_manifest_file = true
-  b.manifest_filename = 'MANIFEST.txt'
   b.use_git           = true
   b.publish[:rubygems] = true
 end
