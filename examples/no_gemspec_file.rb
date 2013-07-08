492a5f1927aaf928ae8c0992dc16d31f1794434c
Rick <rick@cloudscaling.com>
2013-07-07 22:42:37 -0700

    eh, no

--- examples/no_gemspec_file.rb
+++ examples/no_gemspec_file.rb
@@ -1,18 +1,18 @@
 require 'buildar/tasks'
 
 Buildar.conf(__FILE__) do |b|
-                 b.name = 'Example'
-     b.use_gemspec_file = false
-     b.use_version_file = false
-              b.use_git = false
-   b.publish[:rubygems] = false
+  b.name = 'Example'
+  b.use_gemspec_file = false
+  b.use_version_file = false
+  b.use_git = false
+  b.publish[:rubygems] = false
 
-     b.gemspec.summary  = 'Example of foo lorem ipsum'
-     b.gemspec.author   = 'Buildar'
-     b.gemspec.license  = 'MIT'
+  b.gemspec.summary  = 'Example of foo lorem ipsum'
+  b.gemspec.author   = 'Buildar'
+  b.gemspec.license  = 'MIT'
   b.gemspec.description = 'Foo bar baz quux'
-        b.gemspec.files = ['Rakefile']
-      b.gemspec.version = 2.0
+  b.gemspec.files = ['Rakefile']
+  b.gemspec.version = 2.0
 end
 
 # make sure you have a task named :test, even if it's empty
